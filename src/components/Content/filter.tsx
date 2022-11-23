9b7ceb2adf74612b5de21e1dcededd27bbc13d85
z.luo <z.luo@wustl.edu>
2022-11-22 19:11:13 -0800

    Add test data.

--- src/components/Content/filter.tsx
+++ src/components/Content/filter.tsx
@@ -66,7 +66,7 @@ function Filters({ folderData, open, close }: IFilerSection): JSX.Element {
           const data = folderData[v.toLowerCase()] as string[];
           const filter = filters[v.toLowerCase()] as string[];
           return (
-            <div key={v} style={{width: '100%'}}>
+            <div key={v} style={{ width: '100%' }}>
               <Stack
                 direction="row"
                 justifyContent="space-between"
