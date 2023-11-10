47ffe31d6f31a7feb5aed387bd0e2725b11336aa
zluo01 <potterx20a@gmail.com>
2023-11-10 14:36:07 -0800

    lazy load on setting page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import Layout from '@/components/Layout';
 import Home from '@/components/Main';
-import Setting from '@/components/Setting';
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
+const Setting = lazy(() => import('@/components/Setting'));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="setting" element={<Setting />} />
+        <Route
+          path="setting"
+          element={
+            <Suspense>
+              <Setting />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
