fea85ee15d966b6ee8cae4a92aa74d34641feb0e
zluo01 <potterx20a@gmail.com>
2023-11-10 14:14:12 -0800

    Load img with thumbnail use css only.

--- src/components/ImageLoader/poster.tsx
+++ src/components/ImageLoader/poster.tsx
@@ -1,5 +1,5 @@
+import classNames from '@/lib/utils';
 import { CoverType, IImageLoaderPops } from '@/type';
-import { effect, useSignal } from '@preact/signals-react';
 import { ReactElement } from 'react';
 
 function Poster({
@@ -8,25 +8,16 @@ function Poster({
   t,
   ...props
 }: IImageLoaderPops): ReactElement {
-  const imgSrc = useSignal(thumbnail);
-
-  effect(() => {
-    const img = new Image();
-    img.src = cover;
-    img.onload = () => {
-      imgSrc.value = cover;
-    };
-  });
-
   return (
     <img
-      className={
-        t === CoverType.POSTER ? 'h-full w-full object-cover' : 'h-full w-auto'
-      }
-      src={imgSrc.value}
+      className={classNames(
+        t === CoverType.POSTER ? 'w-full object-cover' : 'w-auto',
+        'max-w-full bg-no-repeat bg-cover italic align-middle h-full',
+      )}
+      src={cover}
+      style={{ backgroundImage: `url(${thumbnail})` }}
       {...props}
       alt={props.alt}
-      loading="lazy"
     />
   );
 }
