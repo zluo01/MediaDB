bc1cc87477784e7be9e52ce15f8d5d128a2ce65d
zluo01 <potterx20a@gmail.com>
2024-08-03 00:27:20 -0700

    Prevent terminal window creation on windows.

--- src-tauri/src/parser/utilities.rs
+++ src-tauri/src/parser/utilities.rs
@@ -1,11 +1,10 @@
 use std::{
     fs,
     path::Path,
-    path::PathBuf
-
-    ,
-    process::Command
+    path::PathBuf,
+    process::Command,
 };
+use std::process::Output;
 
 pub fn get_relative_path(path: &Path, base: &Path) -> Option<PathBuf> {
     let relative_path = path.strip_prefix(base);
@@ -15,22 +14,39 @@ pub fn get_relative_path(path: &Path, base: &Path) -> Option<PathBuf> {
     None
 }
 
+#[cfg(target_os = "windows")]
+fn execute_command(args: &Vec<&str>) -> Output {
+    use std::os::windows::process::CommandExt;
+    std::process::Command("ffmpeg")
+        .args(args)
+        // https://learn.microsoft.com/en-us/windows/win32/procthread/process-creation-flags
+        .creation_flags(0x08000000)
+        .output()
+        .expect("Failed to execute command")
+}
+
+fn execute_command(args: &Vec<&str>) -> Output {
+    Command::new("ffmpeg")
+        .args(args)
+        .output()
+        .expect("Failed to execute command")
+}
+
 pub fn convert_image(src_path: &str, dst_path: &str) -> Result<(), String> {
     let dst_conversion_path = format!("{}.avif", dst_path);
 
-    let output = Command::new("ffmpeg")
-        .args(&[
-            "-y",
-            "-i",
-            src_path,
-            "-c:v", "libsvtav1", "-an",
-            "-preset", "4",
-            "-vf", "scale='if(gt(a,320/480),320,-2)':'if(gt(a,320/480),-2,480)'",
-            "-loglevel", "fatal",
-            &dst_conversion_path
-        ])
-        .output()
-        .expect("Failed to execute command");
+    let args = vec![
+        "-y",
+        "-i",
+        src_path,
+        "-c:v", "libsvtav1", "-an",
+        "-preset", "4",
+        "-vf", "scale='if(gt(a,320/480),320,-2)':'if(gt(a,320/480),-2,480)'",
+        "-loglevel", "fatal",
+        &dst_conversion_path,
+    ];
+
+    let output = execute_command(&args);
 
     if !&output.status.success() {
         return Err(format!("Fail to covert image. Raising error {}", String::from_utf8_lossy(&output.stderr)));
