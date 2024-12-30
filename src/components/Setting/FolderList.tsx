f8b559db12983124677dc3f3639e5c864b7829e8
zluo01 <potterx20a@gmail.com>
2024-12-29 23:02:02 -0800

    migrate from dnd to framer motion for drag and drop.

--- src/components/Setting/FolderList.tsx
+++ src/components/Setting/FolderList.tsx
@@ -4,23 +4,86 @@ import { useGetFolderListQuery, useRemoveFolderTrigger } from '@/lib/queries';
 import { updateFolderList } from '@/lib/storage';
 import { IFolder, ModalType } from '@/type';
 import {
-  DragDropContext,
-  Draggable,
-  DraggableProvided,
-  Droppable,
-  DroppableProvided,
-  DropResult,
-} from '@hello-pangea/dnd';
-import {
   FolderIcon,
   PencilSquareIcon,
   TrashIcon,
 } from '@heroicons/react/24/solid';
-import { Fragment, lazy, ReactElement, Suspense, useState } from 'react';
+import { Reorder, useDragControls } from 'motion/react';
+import {
+  lazy,
+  MutableRefObject,
+  ReactElement,
+  Suspense,
+  useRef,
+  useState,
+} from 'react';
 
 const EditFolderModal = lazy(() => import('@/components/Modal/Folder'));
 
+interface IFolderItemProps {
+  folder: IFolder;
+  open: (id: number) => void;
+  remove: (folder: IFolder) => void;
+  container: MutableRefObject<null>;
+}
+
+function FolderItem({
+  folder,
+  open,
+  remove,
+  container,
+}: IFolderItemProps): ReactElement {
+  const controls = useDragControls();
+
+  return (
+    <Reorder.Item
+      key={folder.name}
+      className="flex w-full cursor-grab flex-row items-center justify-between p-2"
+      value={folder}
+      whileDrag={{
+        scale: 1.05,
+        boxShadow: '0px 4px 10px rgba(0,0,0,0.3)',
+      }}
+      dragListener={false}
+      dragControls={controls}
+      dragConstraints={container}
+      dragElastic={0.1}
+    >
+      <div
+        className="flex grow flex-row items-center text-secondary"
+        onPointerDown={e => controls.start(e)}
+      >
+        <FolderIcon className="mr-2 size-10" />
+        <div className="flex flex-col items-start justify-around ">
+          <span className="truncate text-primary">{folder.name}</span>
+          <span className="truncate text-sm">{folder.path}</span>
+        </div>
+      </div>
+      <div className="flex flex-row flex-nowrap items-center justify-end">
+        <button
+          onClick={() => open(folder.position)}
+          type="button"
+          className="inline-flex items-center rounded-lg bg-none p-2.5 text-center text-sm font-medium text-secondary hover:text-hover focus:outline-none focus:ring-0"
+        >
+          <PencilSquareIcon className="size-6" />
+          <span className="sr-only">Add new folder</span>
+        </button>
+        <button
+          onClick={() => remove(folder)}
+          type="button"
+          className="inline-flex items-center rounded-lg bg-none p-2.5 text-center text-sm font-medium text-secondary hover:text-hover focus:outline-none focus:ring-0"
+        >
+          <TrashIcon className="size-6" />
+          <span className="sr-only">Add new folder</span>
+        </button>
+      </div>
+    </Reorder.Item>
+  );
+}
+
 function FolderList(): ReactElement {
+  const container = useRef(null);
+
   const { openModal } = useModalStore();
 
   const { data: folderList, mutate: revalidateFolderList } =
@@ -29,15 +92,12 @@ function FolderList(): ReactElement {
 
   const [folderIndex, setFolderIndex] = useState(-1);
 
-  function reorder(list: IFolder[], startIndex: number, endIndex: number) {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-
-    return result;
+  function open(id: number) {
+    setFolderIndex(id);
+    openModal(ModalType.EDIT_FOLDER);
   }
 
-  async function handleRemove(folder: IFolder) {
+  async function remove(folder: IFolder) {
     try {
       await trigger(folder);
     } catch (e) {
@@ -45,98 +105,42 @@ function FolderList(): ReactElement {
     }
   }
 
-  async function onDragEnd(result: DropResult) {
+  async function onDragEnd(newOrder: IFolder[]) {
     // dropped outside the list
-    if (!folderList || !result.destination) {
+    if (!folderList || !newOrder) {
       return;
     }
-
-    const src = result.source.index;
-    const dst = result.destination.index;
-
-    if (result.source.index !== result.destination.index) {
-      try {
-        const result = reorder(folderList, src, dst);
-        await updateFolderList(result);
-        await revalidateFolderList();
-      } catch (e) {
-        await notify(`Drag End: ${e}`);
-      }
+    try {
+      await updateFolderList(newOrder);
+      await revalidateFolderList();
+    } catch (e) {
+      await notify(`Drag End: ${e}`);
     }
-  }
-
-  function open(id: number) {
-    setFolderIndex(id);
-    openModal(ModalType.EDIT_FOLDER);
   }
 
   return (
-    <Fragment>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable">
-          {(dropProvided: DroppableProvided) => (
-            <ul
-              className="w-full"
-              ref={dropProvided.innerRef}
-              {...dropProvided.droppableProps}
-            >
-              {folderList?.map(folder => (
-                <Draggable
-                  key={folder.name}
-                  draggableId={folder.name}
-                  index={folder.position}
-                >
-                  {(dragProvided: DraggableProvided) => (
-                    <div
-                      ref={dragProvided.innerRef}
-                      {...dragProvided.draggableProps}
-                      {...dragProvided.dragHandleProps}
-                      style={{ ...dragProvided.draggableProps.style }}
-                    >
-                      <li className="flex w-full flex-row items-center justify-between p-2">
-                        <div className="flex grow flex-row items-center text-secondary">
-                          <FolderIcon className="mr-2 size-10" />
-                          <div className="flex flex-col items-start justify-around ">
-                            <span className="truncate text-primary">
-                              {folder.name}
-                            </span>
-                            <span className="truncate text-sm">
-                              {folder.path}
-                            </span>
-                          </div>
-                        </div>
-                        <div className="flex flex-row flex-nowrap items-center justify-end">
-                          <button
-                            onClick={() => open(folder.position)}
-                            type="button"
-                            className="inline-flex items-center rounded-lg bg-none p-2.5 text-center text-sm font-medium text-secondary hover:text-hover focus:outline-none focus:ring-0"
-                          >
-                            <PencilSquareIcon className="size-6" />
-                            <span className="sr-only">Add new folder</span>
-                          </button>
-                          <button
-                            onClick={() => handleRemove(folder)}
-                            type="button"
-                            className="inline-flex items-center rounded-lg bg-none p-2.5 text-center text-sm font-medium text-secondary hover:text-hover focus:outline-none focus:ring-0"
-                          >
-                            <TrashIcon className="size-6" />
-                            <span className="sr-only">Add new folder</span>
-                          </button>
-                        </div>
-                      </li>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {dropProvided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-      </DragDropContext>
+    <>
+      <Reorder.Group
+        className="flex w-full select-none flex-col flex-nowrap"
+        values={folderList || []}
+        axis="y"
+        onReorder={onDragEnd}
+        ref={container}
+      >
+        {folderList?.map(folder => (
+          <FolderItem
+            key={folder.name}
+            folder={folder}
+            open={open}
+            remove={remove}
+            container={container}
+          />
+        ))}
+      </Reorder.Group>
       <Suspense>
         <EditFolderModal index={folderIndex} />
       </Suspense>
-    </Fragment>
+    </>
   );
 }
 
