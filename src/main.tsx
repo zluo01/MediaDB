8d55b8a42a6b47bbab87f3b4df483e77b42f2c81
zluo01 <12102011+zluo01@users.noreply.github.com>
2026-02-13 22:58:05 -0800

    docs: update comment from React Query to TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ const router = createRouter({
 	},
 	scrollRestoration: true,
 	defaultPreload: 'intent',
-	// Since we're using React Query, we don't want loader calls to ever be stale
+	// Since we're using TanStack Query, we don't want loader calls to ever be stale
 	// This will ensure that the loader is always called when the route is preloaded or visited
 	defaultPreloadStaleTime: 0,
 });
