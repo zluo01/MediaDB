da2cf2ff310ca841986b1438417aeaade8557abf
zluo01 <12102011+zluo01@users.noreply.github.com>
2026-03-01 14:53:59 -0800

    fix: typo in media_year_desc index name

--- src-tauri/src/db/queries.rs
+++ src-tauri/src/db/queries.rs
@@ -44,7 +44,7 @@ pub const CREAT_TABLE_QUERY: &str = "
         on media (title desc);
     create index media_year_asc_index
         on media (year);
-    create index media_year_desc__index
+    create index media_year_desc_index
         on media (year desc);
     CREATE INDEX media_folder_index ON media(folder);
     create table if not exists tags
