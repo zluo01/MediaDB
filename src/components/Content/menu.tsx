e8c6a76556440546671f0b1759678e84f8e04020
zluo01 <12102011+zluo01@users.noreply.github.com>
2026-03-29 21:08:13 -0700

    perf: replace Switch+For+Match with Show to render only the active season tab

--- src/components/Content/menu.tsx
+++ src/components/Content/menu.tsx
@@ -1,4 +1,4 @@
-import { createSignal, For, Match, Switch } from 'solid-js';
+import { createSignal, For, Show } from 'solid-js';
 import Poster from '@/components/ImageLoader/poster';
 import { openFile } from '@/lib/os';
 import { cn } from '@/lib/utils';
@@ -36,38 +36,34 @@ export default function TVShowCardMenu(props: ITVShowCardMenuProps) {
 					</For>
 				</div>
 				<div class="h-[38.2vh] w-full p-6">
-					<Switch>
-						<For each={seasonKeys()}>
-							{(season, index) => (
-								<Match when={index() === active()}>
-									<div class="flex flex-row flex-nowrap justify-around">
-										<Poster
-											src={
-												props.media.posters[season] || props.media.posters.main
-											}
-											alt={props.media.title}
-											width={240}
-											height={320}
-										/>
-										<div class="grid h-full w-[61.8vw] auto-cols-min auto-rows-min grid-cols-10 gap-1">
-											<For each={props.media.seasons[season]}>
-												{episode => (
-													<div class="tooltip" data-tip={episode.title}>
-														<div
-															onClick={() => openEpisodeFile(episode)}
-															class="btn btn-outline btn-wide"
-														>
-															{episode.episode}
-														</div>
-													</div>
-												)}
-											</For>
-										</div>
-									</div>
-								</Match>
-							)}
-						</For>
-					</Switch>
+					<Show when={seasonKeys()[active()]}>
+						{season => (
+							<div class="flex flex-row flex-nowrap justify-around">
+								<Poster
+									src={
+										props.media.posters[season()] || props.media.posters.main
+									}
+									alt={props.media.title}
+									width={240}
+									height={320}
+								/>
+								<div class="grid h-full w-[61.8vw] auto-cols-min auto-rows-min grid-cols-10 gap-1">
+									<For each={props.media.seasons[season()]}>
+										{episode => (
+											<div class="tooltip" data-tip={episode.title}>
+												<div
+													onClick={() => openEpisodeFile(episode)}
+													class="btn btn-outline btn-wide"
+												>
+													{episode.episode}
+												</div>
+											</div>
+										)}
+									</For>
+								</div>
+							</div>
+						)}
+					</Show>
 				</div>
 			</div>
 			<form method="dialog" class="modal-backdrop">
